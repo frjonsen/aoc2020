0c18068caa070cc285c46c5be25f0f6d60ea38b6
Fredrik Jonsén <fredrik@jonsen.se>
2020-12-24 12:26:40 +0100

    day 12, part 2

--- src/day12.rs
+++ src/day12.rs
@@ -1,3 +1,5 @@
+use std::borrow::Borrow;
+
 enum Instruction {
     North(i64),
     West(i64),
@@ -8,12 +10,67 @@ enum Instruction {
     Forward(i64),
 }
 
+#[derive(Eq, PartialEq, Debug)]
 struct Coordinate {
     x: i64,
     y: i64,
 }
 
+impl Coordinate {
+    pub fn rotate(&self, degrees: i64) -> Self {
+        match degrees {
+            180 => Coordinate {
+                x: -self.x,
+                y: -self.y,
+            },
+            90 => Coordinate {
+                x: self.y,
+                y: -self.x,
+            },
+            270 => Coordinate {
+                x: -self.y,
+                y: self.x,
+            },
+            _ => panic!("Not a 90 degree rotation"),
+        }
+    }
+}
+
+impl std::ops::Add for &Coordinate {
+    type Output = Coordinate;
+    fn add(self, other: &Coordinate) -> Self::Output {
+        Coordinate {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl<B: Borrow<Coordinate>> std::ops::AddAssign<B> for Coordinate {
+    fn add_assign(&mut self, other: B) {
+        self.x += other.borrow().x;
+        self.y += other.borrow().y;
+    }
+}
+
+impl std::ops::Mul<i64> for &Coordinate {
+    type Output = Coordinate;
+    fn mul(self, rhs: i64) -> Self::Output {
+        Coordinate {
+            x: self.x * rhs,
+            y: self.y * rhs,
+        }
+    }
+}
+
 impl Ship {
+    pub fn new() -> Self {
+        Self {
+            facing: 90,
+            position: Coordinate { x: 0, y: 0 },
+        }
+    }
+
     pub fn travel(&mut self, instruction: &Instruction) {
         match instruction {
             Instruction::North(_)
@@ -62,12 +119,51 @@ struct Ship {
     pub position: Coordinate,
 }
 
-impl Ship {
+struct Ship2 {
+    pub position: Coordinate,
+    pub waypoint: Coordinate,
+}
+
+impl Ship2 {
     pub fn new() -> Self {
         Self {
-            facing: 90,
             position: Coordinate { x: 0, y: 0 },
+            waypoint: Coordinate { x: 10, y: 1 },
         }
+    }
+
+    pub fn perform_instruction(&mut self, instruction: &Instruction) {
+        match instruction {
+            Instruction::North(_)
+            | Instruction::East(_)
+            | Instruction::West(_)
+            | Instruction::South(_) => self.move_waypoint(instruction),
+            Instruction::Left(_) | Instruction::Right(_) => self.rotate(instruction),
+            Instruction::Forward(d) => self.forward(d),
+        }
+    }
+
+    pub fn rotate(&mut self, instruction: &Instruction) {
+        let degree = match instruction {
+            &Instruction::Left(d) => 360 - d,
+            &Instruction::Right(d) => d,
+            _ => panic!("Not a rotation instruction"),
+        };
+        self.waypoint = self.waypoint.rotate(degree);
+    }
+
+    pub fn move_waypoint(&mut self, instruction: &Instruction) {
+        match instruction {
+            Instruction::North(d) => self.waypoint.y += d,
+            Instruction::South(d) => self.waypoint.y -= d,
+            Instruction::West(d) => self.waypoint.x -= d,
+            Instruction::East(d) => self.waypoint.x += d,
+            _ => panic!("Not a direction instruction"),
+        }
+    }
+
+    pub fn forward(&mut self, multiplier: &i64) {
+        self.position += &self.waypoint * *multiplier;
     }
 }
 
@@ -105,9 +201,19 @@ fn day12_part1(input: &Vec<Instruction>) -> u64 {
     ship.position.x.abs() as u64 + ship.position.y.abs() as u64
 }
 
+#[aoc(day12, part2)]
+fn day12_part2(input: &Vec<Instruction>) -> u64 {
+    let mut ship = Ship2::new();
+    for i in input {
+        ship.perform_instruction(i);
+    }
+
+    ship.position.x.abs() as u64 + ship.position.y.abs() as u64
+}
+
 #[cfg(test)]
 mod tests {
-    use super::{day12_part1, input_generator, Instruction, Ship};
+    use super::{day12_part1, input_generator, Coordinate, Instruction, Ship, Ship2};
 
     #[test]
     fn test_rotate_right() {
@@ -132,5 +238,13 @@ mod tests {
         let instructions = input_generator(input);
         let res = day12_part1(&instructions);
         assert_eq!(res, 25);
+    }
+
+    #[test]
+    pub fn test_rotate_waypoint() {
+        let mut ship = Ship2::new();
+        ship.waypoint = Coordinate { x: 10, y: 4 };
+        ship.rotate(&Instruction::Right(90));
+        assert_eq!(ship.waypoint, Coordinate { x: 4, y: -10 });
     }
 }
