67ab9cffd0d6058a54e4c745dd5e2eda7a6957d8
Fredrik Jonsén <frjonsen@users.noreply.github.com>
2021-09-02 21:00:08 +0200

    finish day 14

--- src/day14.rs
+++ src/day14.rs
@@ -5,6 +5,8 @@ enum Operation {
     SetMask { mask: String },
     SetMem { adress: usize, value: u64 },
 }
+
+#[derive(Debug)]
 struct Mask {
     set: u64,
     unset: u64,
@@ -64,9 +66,59 @@ fn day14_part1(input: &[Operation]) -> u64 {
     stack.values().sum()
 }
 
+fn replace_x(indices: &[usize], adress: &str) -> Vec<String> {
+    if indices.is_empty() {
+        return vec![adress.to_owned()];
+    }
+    let index = indices[0];
+
+    let mut one_replacement = adress.to_owned();
+    let mut zero_replacement = one_replacement.clone();
+    one_replacement.replace_range(index..(index + 1), "1");
+    zero_replacement.replace_range(index..(index + 1), "0");
+    let one_branch = replace_x(&indices[1..], &one_replacement);
+    let zero_branch = replace_x(&indices[1..], &zero_replacement);
+
+    one_branch
+        .into_iter()
+        .chain(zero_branch.into_iter())
+        .collect()
+}
+
+fn set_at_adress(stack: &mut HashMap<usize, u64>, mask: &str, adress: usize, value: u64) {
+    let adress = adress | usize::from_str_radix(&mask.replace('X', "0"), 2).unwrap();
+    let adress = format!("{:036b}", adress);
+    let indices: Vec<usize> = mask.match_indices('X').map(|c| c.0).collect();
+
+    let adresses = replace_x(&indices, &adress)
+        .into_iter()
+        .map(|a| usize::from_str_radix(&a, 2).unwrap());
+
+    for adress in adresses {
+        stack.insert(adress, value);
+    }
+}
+
+#[aoc(day14, part2)]
+fn day14_part2(input: &[Operation]) -> u64 {
+    let mut current_mask = String::new();
+
+    let mut stack: HashMap<usize, u64> = HashMap::new();
+    for op in input {
+        match op {
+            Operation::SetMask { mask } => current_mask = mask.clone(),
+            Operation::SetMem { adress, value } => {
+                set_at_adress(&mut stack, &current_mask, *adress, *value)
+            }
+        }
+    }
+
+    stack.values().sum()
+}
+
 #[cfg(test)]
 mod tests {
-    use super::{day14_part1, input_generator, to_mask};
+    use super::{day14_part1, day14_part2, input_generator, to_mask};
 
     #[test]
     fn test_parse_mask() {
@@ -86,5 +138,16 @@ mod tests {
         let generated = input_generator(input);
         let res = day14_part1(&generated);
         assert_eq!(res, 165);
+    }
+
+    #[test]
+    fn test_given_part2() {
+        let input = "mask = 000000000000000000000000000000X1001X
+            mem[42] = 100
+            mask = 00000000000000000000000000000000X0XX
+            mem[26] = 1";
+        let generated = input_generator(input);
+        let res = day14_part2(&generated);
+        assert_eq!(res, 208);
     }
 }
