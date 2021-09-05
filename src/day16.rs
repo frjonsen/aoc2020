2dd2ca2c91b105f8f1ed0a57d16ec6e0a5b57867
Fredrik Jonsén <frjonsen@users.noreply.github.com>
2021-09-05 14:52:35 +0200

    Finish day 16 with horribly ugly solution

--- src/day16.rs
+++ src/day16.rs
@@ -74,5 +74,129 @@ fn input_generator(input: &str) -> Input {
 
 #[aoc(day16, part1)]
 fn day16_part1(input: &Input) -> u32 {
-    5
+    let all_rules: Vec<_> = input
+        .rules
+        .iter()
+        .map(|r| [&r.interval_one, &r.interval_two])
+        .flatten()
+        .collect();
+
+    input
+        .nearby_tickets
+        .iter()
+        .flatten()
+        .filter(|v| !all_rules.iter().any(|p| p.contains(v)))
+        .sum()
+}
+
+#[aoc(day16, part2)]
+fn day16_part2(input: &Input) -> u64 {
+    let all_rules: Vec<_> = input
+        .rules
+        .iter()
+        .map(|r| [&r.interval_one, &r.interval_two])
+        .flatten()
+        .collect();
+
+    let valid_tickets: Vec<&Vec<u32>> = input
+        .nearby_tickets
+        .iter()
+        .filter(|ticket| {
+            ticket
+                .iter()
+                .all(|v| all_rules.iter().any(|r| r.contains(v)))
+        })
+        .collect();
+
+    let slot_count = valid_tickets.first().unwrap().len();
+
+    let mut valid_for_slot = vec![input.rules.iter().collect::<Vec<&Rule>>(); slot_count];
+
+    for ticket in &valid_tickets {
+        for i in 0..slot_count {
+            let still_valid = valid_for_slot
+                .get(i)
+                .unwrap()
+                .iter()
+                .filter(|r| {
+                    r.interval_one.contains(&ticket[i]) || r.interval_two.contains(&ticket[i])
+                })
+                .copied()
+                .collect::<Vec<_>>();
+            if still_valid.len() == 1 {
+                let name = &still_valid.first().unwrap().name;
+                filter_determined(&mut valid_for_slot, name);
+            }
+            valid_for_slot[i] = still_valid;
+        }
+    }
+    for ticket in &valid_tickets {
+        for i in 0..slot_count {
+            let still_valid = valid_for_slot
+                .get(i)
+                .unwrap()
+                .iter()
+                .filter(|r| {
+                    r.interval_one.contains(&ticket[i]) || r.interval_two.contains(&ticket[i])
+                })
+                .copied()
+                .collect::<Vec<_>>();
+            if still_valid.len() == 1 {
+                let name = &still_valid.first().unwrap().name;
+                filter_determined(&mut valid_for_slot, name);
+            }
+            valid_for_slot[i] = still_valid;
+        }
+    }
+
+    let departure_slots: Vec<usize> = valid_for_slot
+        .iter()
+        .map(|v| v.first())
+        .map(Option::unwrap)
+        .copied()
+        .enumerate()
+        .filter(|r| r.1.name.starts_with("departure"))
+        .map(|r| r.0)
+        .collect();
+
+    departure_slots
+        .iter()
+        .map(|r| input.personal_ticket.get(*r))
+        .map(Option::unwrap)
+        .map(|r| *r as u64)
+        .product()
+}
+
+fn filter_determined(rules: &mut Vec<Vec<&Rule>>, name: &str) {
+    for slot_rules in rules.iter_mut() {
+        if slot_rules.len() == 1 {
+            continue;
+        }
+        slot_rules.retain(|r| r.name != name);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{day16_part1, input_generator};
+
+    #[test]
+    fn test_given_day16() {
+        let input = "class: 1-3 or 5-7
+        row: 6-11 or 33-44
+        seat: 13-40 or 45-50
+
+        your ticket:
+        7,1,14
+
+        nearby tickets:
+        7,3,47
+        40,4,50
+        55,2,20
+        38,6,12";
+
+        let generated = input_generator(&input);
+        let res = day16_part1(&generated);
+        assert_eq!(res, 71);
+    }
 }
