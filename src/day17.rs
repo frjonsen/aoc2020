22e35ddd0d2adc4e71dbd3a43c7d5d846ae4d69c
Fredrik Jonsén <frjonsen@users.noreply.github.com>
2021-09-07 22:09:20 +0200

    Day 17 completed with copypaste solution

--- src/day17.rs
+++ src/day17.rs
@@ -19,42 +19,38 @@ fn input_generator(input: &str) -> Vec<Vec<bool>> {
 
 #[derive(PartialEq)]
 struct Coordinate {
-    x: i64,
-    y: i64,
-    z: i64,
+    parts: Vec<i64>,
 }
 
-fn calculate_neighbors(coord: &Coordinate, edge: usize) -> Vec<Coordinate> {
-    let mut neighbors = vec![];
+fn calculate_neighbors(coord: &Coordinate, edge: usize, neighbors: &[Vec<i64>]) -> Vec<Coordinate> {
+    let mut to_check = Vec::with_capacity(coord.parts.len().pow(coord.parts.len() as u32));
 
-    let modifiers = &[-1, 1];
-    let e = edge.try_into().unwrap();
+    let e = edge as i64;
 
-    for x in -1..=1 {
-        for y in -1..=1 {
-            for z in -1..=1 {
-                if x == 0 && y == 0 && z == 0 {
-                    continue;
-                }
-                if coord.x + x < 0 || coord.x + x >= e {
-                    continue;
-                }
-                if coord.y + y < 0 || coord.y + y >= e {
-                    continue;
-                }
-                if coord.z + z < 0 || coord.z + z >= e {
-                    continue;
-                }
-                neighbors.push(Coordinate {
-                    x: coord.x + x,
-                    y: coord.y + y,
-                    z: coord.z + z,
-                });
-            }
+    for parts in neighbors.iter() {
+        if parts.iter().all(|p| *p == 0) {
+            continue;
         }
+
+        if coord
+            .parts
+            .iter()
+            .enumerate()
+            .any(|(i, v)| v + parts[i] < 0 || v + parts[i] >= e)
+        {
+            continue;
+        }
+
+        to_check.push(Coordinate {
+            parts: parts
+                .iter()
+                .enumerate()
+                .map(|(i, v)| coord.parts[i] + v)
+                .collect(),
+        });
     }
 
-    neighbors
+    to_check
 }
 
 fn print_cube_slice(cube: &[Vec<Vec<bool>>], max: usize, z: usize) {
@@ -80,56 +76,117 @@ fn day17_part1(initial_state: &[Vec<bool>]) -> usize {
             cube[x + max / 2][y + max / 2][max / 2] = initial_state[x][y];
         }
     }
+    const DIM: usize = 3;
+
+    let neighbor_checks: Vec<Vec<i64>> = (0..DIM)
+        .map(|_| -1..=1)
+        .multi_cartesian_product()
+        .into_iter()
+        .collect_vec();
+
+    let cells = (0..DIM)
+        .map(|_| 0..max)
+        .multi_cartesian_product()
+        .collect_vec();
 
     for _ in 0..6 {
         let mut new_cube = vec![vec![vec![false; max]; max]; max];
 
-        for x in 0..max {
-            for y in 0..max {
-                for z in 0..max {
-                    let current_neighbors = calculate_neighbors(
-                        &Coordinate {
-                            x: x as i64,
-                            y: y as i64,
-                            z: z as i64,
-                        },
-                        max,
-                    );
-                    let neighbor_active = current_neighbors
-                        .into_iter()
-                        .map(|c| cube[c.x as usize][c.y as usize][c.z as usize])
-                        .filter(|c| *c)
-                        .count();
-                    if cube[x][y][z] {
-                        new_cube[x][y][z] = neighbor_active == 2 || neighbor_active == 3;
-                    } else {
-                        new_cube[x][y][z] = neighbor_active == 3;
-                    }
-                }
+        for coord_parts in cells.iter() {
+            let current_neighbors = calculate_neighbors(
+                &Coordinate {
+                    parts: coord_parts.iter().map(|c| *c as i64).collect(),
+                },
+                max,
+                &neighbor_checks,
+            );
+            let neighbor_active = current_neighbors
+                .into_iter()
+                .map(|c| cube[c.parts[0] as usize][c.parts[1] as usize][c.parts[2] as usize])
+                .filter(|c| *c)
+                .count();
+            if cube[coord_parts[0]][coord_parts[1]][coord_parts[2]] {
+                new_cube[coord_parts[0]][coord_parts[1]][coord_parts[2]] =
+                    neighbor_active == 2 || neighbor_active == 3;
+            } else {
+                new_cube[coord_parts[0]][coord_parts[1]][coord_parts[2]] = neighbor_active == 3;
             }
         }
+
         cube = new_cube;
     }
 
-    print_cube_slice(&cube, max, max / 2);
-
-    let mut sum = 0;
-    for x in 0..max {
-        for y in 0..max {
-            for z in 0..max {
-                if cube[x][y][z] {
-                    sum += 1;
-                }
-            }
-        }
-    }
-
-    sum
+    (0..DIM)
+        .map(|_| 0..max)
+        .multi_cartesian_product()
+        .filter(|c| cube[c[0]][c[1]][c[2]])
+        .count()
 }
 
+#[aoc(day17, part2)]
+fn day17_part2(initial_state: &[Vec<bool>]) -> usize {
+    let max: usize = (initial_state.len() + 6) * 2;
+
+    let mut cube = vec![vec![vec![vec![false; max]; max]; max]; max];
+    for x in 0..initial_state.len() {
+        for y in 0..initial_state[x].len() {
+            cube[x + max / 2][y + max / 2][max / 2][max / 2] = initial_state[x][y];
+        }
+    }
+    const DIM: usize = 4;
+
+    let neighbor_checks: Vec<Vec<i64>> = (0..DIM)
+        .map(|_| -1..=1)
+        .multi_cartesian_product()
+        .into_iter()
+        .collect_vec();
+
+    let cells = (0..DIM)
+        .map(|_| 0..max)
+        .multi_cartesian_product()
+        .collect_vec();
+
+    for _ in 0..6 {
+        let mut new_cube = vec![vec![vec![vec![false; max]; max]; max]; max];
+
+        for coord_parts in cells.iter() {
+            let current_neighbors = calculate_neighbors(
+                &Coordinate {
+                    parts: coord_parts.iter().map(|c| *c as i64).collect(),
+                },
+                max,
+                &neighbor_checks,
+            );
+            let neighbor_active = current_neighbors
+                .into_iter()
+                .map(|c| {
+                    cube[c.parts[0] as usize][c.parts[1] as usize][c.parts[2] as usize]
+                        [c.parts[3] as usize]
+                })
+                .filter(|c| *c)
+                .count();
+            if cube[coord_parts[0]][coord_parts[1]][coord_parts[2]][coord_parts[3]] {
+                new_cube[coord_parts[0]][coord_parts[1]][coord_parts[2]][coord_parts[3]] =
+                    neighbor_active == 2 || neighbor_active == 3;
+            } else {
+                new_cube[coord_parts[0]][coord_parts[1]][coord_parts[2]][coord_parts[3]] =
+                    neighbor_active == 3;
+            }
+        }
+
+        cube = new_cube;
+    }
+
+    (0..DIM)
+        .map(|_| 0..max)
+        .multi_cartesian_product()
+        .filter(|c| cube[c[0]][c[1]][c[2]][c[3]])
+        .count()
+}
 #[cfg(test)]
 mod tests {
-    use super::{calculate_neighbors, day17_part1, input_generator, Coordinate};
+    use super::{day17_part1, input_generator};
+    use itertools::Itertools;
 
     #[test]
     fn test_day17_part_1_given() {
@@ -142,26 +199,8 @@ mod tests {
     }
 
     #[test]
-    fn test_calculate_neighbors() {
-        let coordinate = Coordinate { x: 1, y: 1, z: 1 };
-
-        let res = calculate_neighbors(&coordinate, 5);
-        let mut expected = vec![];
-        for x in -1..=1 {
-            for y in -1..=1 {
-                for z in -1..=1 {
-                    if x == 0 && y == 0 && z == 0 {
-                        continue;
-                    }
-                    expected.push(Coordinate {
-                        x: 1 + x,
-                        y: 1 + y,
-                        z: 1 + z,
-                    });
-                }
-            }
-        }
-        assert_eq!(res.len(), 26);
-        assert!(expected.into_iter().all(|ref e| res.contains(e)));
+    fn test_something() {
+        let t = (0..3).map(|_| 0..18).multi_cartesian_product().count();
+        assert_eq!(t, 18usize.pow(3));
     }
 }
