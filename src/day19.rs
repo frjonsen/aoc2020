4ac2388d083107bfc7606f3b72a56ebb5531953f
Fredrik Jonsén <frjonsen@users.noreply.github.com>
2021-09-30 17:43:52 +0200

    Working fast solution for day 19

--- src/day19.rs
+++ src/day19.rs
@@ -43,7 +43,7 @@ fn get_rule(rule_number: u64, known_rules: &mut KnownRules, raw_rules: &RawRules
         parsed_parts.push(to_concat.join(""));
     }
 
-    let finished_rule = format!("({})", parsed_parts.join("|"));
+    let finished_rule = format!("(?:{})", parsed_parts.join("|"));
 
     known_rules.insert(rule_number, finished_rule.clone());
 
@@ -68,8 +68,6 @@ fn day19_part1(parts: &(Vec<String>, Vec<String>)) -> usize {
 
     let zero_rule = get_rule(0, &mut known_rules, &raw_rules);
 
-    println!("{}", zero_rule);
-
     let zero_rule_pattern = Regex::new(&format!("^{}$", zero_rule)).unwrap();
 
     parts
@@ -79,10 +77,60 @@ fn day19_part1(parts: &(Vec<String>, Vec<String>)) -> usize {
         .count()
 }
 
+#[aoc(day19, part2)]
+fn day19_part2(parts: &(Vec<String>, Vec<String>)) -> usize {
+    let raw_rules: RawRules = parts
+        .0
+        .iter()
+        .map(|l| {
+            let rule_parts: Vec<_> = l.split(':').collect();
+            (
+                rule_parts[0].parse().unwrap(),
+                rule_parts[1].trim().to_owned(),
+            )
+        })
+        .collect();
+
+    let mut known_rules = KnownRules::new();
+
+    let rule_42 = get_rule(42, &mut known_rules, &raw_rules);
+
+    let rule_31 = get_rule(31, &mut known_rules, &raw_rules);
+
+    let rule_31_pattern = Regex::new(&format!("({})", rule_31)).unwrap();
+    let rule_42_pattern = Regex::new(&format!("({})", rule_42)).unwrap();
+
+    let zero_rule_pattern = Regex::new(&format!(
+        "^(?P<ft>(?:{}){{2,}})(?P<to>(?:{})+)$",
+        rule_42, rule_31
+    ))
+    .unwrap();
+
+    let mut matches = 0usize;
+
+    for message in parts.1.iter() {
+        if !zero_rule_pattern.is_match(message) {
+            continue;
+        }
+
+        let captures: Vec<_> = zero_rule_pattern.captures_iter(message).collect();
+        let capture = captures.first().unwrap();
+
+        let to_matches = rule_31_pattern.find_iter(&capture["to"]).count();
+        let ft_matches = rule_42_pattern.find_iter(&capture["ft"]).count();
+
+        if to_matches < ft_matches {
+            matches += 1;
+        }
+    }
+
+    matches
+}
+
 #[cfg(test)]
 mod tests {
-    use super::{get_rule, KnownRules, RawRules};
-    use std::collections::HashMap;
+
+    use super::{day19_part2, get_rule, input_generator, KnownRules, RawRules};
 
     #[test]
     fn test_basic_concat_from_known() {
@@ -133,5 +181,60 @@ mod tests {
         .collect();
         let res = get_rule(1, &mut KnownRules::new(), &raw_rules);
         assert_eq!(res, "(a(a|b)|ba)");
+    }
+
+    #[test]
+    fn test_day_19_given_part_2() {
+        let input = r#"42: 9 14 | 10 1
+        9: 14 27 | 1 26
+        10: 23 14 | 28 1
+        1: "a"
+        11: 42 31
+        5: 1 14 | 15 1
+        19: 14 1 | 14 14
+        12: 24 14 | 19 1
+        16: 15 1 | 14 14
+        31: 14 17 | 1 13
+        6: 14 14 | 1 14
+        2: 1 24 | 14 4
+        0: 8 11
+        13: 14 3 | 1 12
+        15: 1 | 14
+        17: 14 2 | 1 7
+        23: 25 1 | 22 14
+        28: 16 1
+        4: 1 1
+        20: 14 14 | 1 15
+        3: 5 14 | 16 1
+        27: 1 6 | 14 18
+        14: "b"
+        21: 14 1 | 1 14
+        25: 1 1 | 1 14
+        22: 14 14
+        8: 42
+        26: 14 22 | 1 20
+        18: 15 15
+        7: 14 5 | 1 21
+        24: 14 1
+
+        abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+        bbabbbbaabaabba
+        babbbbaabbbbbabbbbbbaabaaabaaa
+        aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+        bbbbbbbaaaabbbbaaabbabaaa
+        bbbababbbbaaaaaaaabbababaaababaabab
+        ababaaaaaabaaab
+        ababaaaaabbbaba
+        baabbaaaabbaaaababbaababb
+        abbbbabbbbaaaababbbbbbaaaababb
+        aaaaabbaabaaaaababaa
+        aaaabbaaaabbaaa
+        aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+        babaaabbbaaabaababbaabababaaab
+        aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba"#;
+
+        let generated = input_generator(input);
+        let res = day19_part2(&generated);
+        assert_eq!(res, 12);
     }
 }
